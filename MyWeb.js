97018db3c18c089b2181dd01558b9d1fec0af7f5
Nerve-98 <rudtjq0331@gmail.com>
2021-02-08 19:26:15 +0900

    project modal

--- MyWeb.js
+++ MyWeb.js
@@ -84,3 +84,79 @@ document.getElementById('firstDot').addEventListener('click', currentImageSlide.
 document.getElementById('secondDot').addEventListener('click', currentImageSlide.bind(null,2));
 document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null,3));
 document.getElementById('forthDot').addEventListener('click', currentImageSlide.bind(null,4));
+
+
+
+
+/* Project area */
+
+filterSelection('all');
+
+function filterSelection(id) {
+    var x, i;
+    x = document.getElementsByClassName('listItem');
+
+    for(i=0;i<x.length;i++){
+        removeClass(x[i], 'active');
+    }
+
+    addClass(document.getElementById(id), 'active');
+    x = document.getElementsByClassName('filterItem');
+
+    if(id == 'all') id = '';
+    for(i=0;i<x.length;i++){
+        removeClass(x[i], 'show');
+        if(x[i].className.indexOf(id) > -1)
+        addClass(x[i], 'show');
+    }
+}
+
+function addClass(element, name) {
+    if(element.className.indexOf(name) == -1) {
+        element.className += " " + name;
+    }
+}
+
+function removeClass(element, name) {
+    var arr;
+    arr = element.className.split(" ");
+
+    while(arr.indexOf(name) > -1){
+        arr.splice(arr.indexOf(name), 1);
+    }
+
+    element.className = arr.join(" ");
+}
+
+document.getElementById('all').addEventListener('click', filterSelection.bind(null, 'all'));
+document.getElementById('db').addEventListener('click', filterSelection.bind(null, 'db'));
+document.getElementById('java').addEventListener('click', filterSelection.bind(null, 'java'));
+document.getElementById('ml').addEventListener('click', filterSelection.bind(null, 'ml'));
+function viewPortfolio(event) {
+    var polyNode = event.target;
+
+    if(polyNode.tagName.toLowerCase() == 'i') { polyNode = polyNode.parentNode; }
+  
+    var overlayNode = polyNode;
+    var imageNode = overlayNode.nextElementSibling;
+  
+    var itemNode = overlayNode.parentNode;
+    var mainNode = itemNode.nextElementSibling;
+    var subNode = mainNode.nextElementSibling;
+    var textNode = subNode.nextElementSibling;
+  
+    document.getElementById('modalImage').src = imageNode.src;
+    document.getElementById('modalMain').innerHTML = mainNode.innerHTML;
+    document.getElementById('modalSub').innerHTML = subNode.innerHTML;
+    document.getElementById('modalText').innerHTML = textNode.innerHTML;
+    document.getElementById('projectModal').style.display = 'block';
+}
+  
+document.getElementById('modalClose').addEventListener('click', 
+    function(){document.getElementById('projectModal').style.display = 'none';});
+  
+var filterItems = document.getElementsByClassName('overlay');
+  
+for(var i=0;i<filterItems.length;i++){
+    filterItems[i].addEventListener('click', viewPortfolio);
+}
